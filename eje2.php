0bbe4f62150c11f5ad3caecb30a688f227393de6
Octavio <octaviodiaz262@gmail.com>
2023-05-23 17:57:35 -0300

    Ejercicio 1 2 y 3

---
+++ eje2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php $v1 = 5;
+        $v2 = 10;
+        $suma = $v1 + $v2;
+        echo "La suma es $suma";
+        ?>
+</body>
+</html>
